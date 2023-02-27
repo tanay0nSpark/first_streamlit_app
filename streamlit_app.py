33e344c95d0a20d29ae29ff4724320e990af067d
tanay0nSpark <35804220+tanay0nSpark@users.noreply.github.com>
2023-02-27 11:37:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('He he ha haha');
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
